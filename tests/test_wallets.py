1b3231ed639203612bc267a9f7aeac6e0b216f1b
John Gaynes <john.gaynes@colorado.edu>
2025-04-17 22:40:23 -0600

    FIxes to address comments on PR review

--- tests/test_wallets.py
+++ tests/test_wallets.py
@@ -1,18 +1,19 @@
 import unittest
 from app import create_app, db
 from app.models import Users, Wallets
-from flask_testing import TestCase
 
-class TestWalletsModel(TestCase):
-    def create_app(self):
-        app = create_app()
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        return app
-
+class TestWalletsModel(unittest.TestCase):
     def setUp(self):
+        self.app = create_app()
+        self.app.config['TESTING'] = True
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
         db.create_all()
+
         self.user = Users(
             username='testuser',
             first_name='Test',
@@ -26,6 +27,7 @@ class TestWalletsModel(TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_wallet_creation(self):
         wallet = Wallets(user_id=self.user.id, balance=100)
@@ -41,7 +43,7 @@ class TestWalletsModel(TestCase):
         db.session.add(wallet)
         db.session.commit()
 
-        fetched_wallet = Wallets.query.get(wallet.id)
+        fetched_wallet = db.session.get(Wallets, wallet.id)
         self.assertEqual(fetched_wallet.balance, 0)
 
     def test_wallet_balance_cannot_be_negative(self):
@@ -55,7 +57,7 @@ class TestWalletsModel(TestCase):
         db.session.add(wallet)
         db.session.commit()
 
-        self.assertEqual(len(self.user.wallet), 1)  # List with one element
+        # Handle one-to-many list case
         self.assertEqual(self.user.wallet[0].balance, 50)
 
     def test_user_wallet_uniqueness(self):
@@ -65,7 +67,6 @@ class TestWalletsModel(TestCase):
         db.session.add(wallet2)
         with self.assertRaises(Exception):
             db.session.commit()
-
 
 if __name__ == '__main__':
     unittest.main()
